545ee6d7d021033515f396866f3c007a18a93517
Maya Thompson <majsant96@hotmail.com>
2024-11-05 14:12:42 +0100

    create js module files and add module type on js script tag

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navBox = document.getElementById('nav-box');
-    const header = document.querySelector('header');
-  
-    navBox.addEventListener('click', function() {
-        header.classList.toggle('active');
-    });
+// Toggle nav bar color for mobile screens
+document.addEventListener("DOMContentLoaded", function () {
+  const navBox = document.getElementById("nav-box");
+  const header = document.querySelector("header");
+
+  navBox.addEventListener("click", function () {
+    header.classList.toggle("active");
+  });
 });
